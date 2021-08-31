7f04d3f778f923ba1bfba499d32a4a46a65a0b99
Elisa Nicot <nico0122@algonquinlive.com>
2021-08-31 11:19:06 -0400

    Add some space

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var a = prompt('Enter a letter');
 var i;
-var line = a.repeat(i +1);
+var line = a.repeat(i + 1);
 
 var writeLetterManyTimes = function () {
 
   for (i = 0; i < 10; i++) {
-    document.write(line +'<br>');
+    document.write(line + '<br>');
   }
 };
 
