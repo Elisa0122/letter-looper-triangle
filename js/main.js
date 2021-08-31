e8949c28cf45256bd4fbc1eca6ee2561bd403485
Elisa Nicot <nico0122@algonquinlive.com>
2021-08-31 11:09:48 -0400

    Update main.js page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var writeLetterManyTimes = function () {
+ar writeLetterManyTimes2 = function () {
   var a = prompt('Enter a letter');
   var i;
 
   for (i = 0; i < 10; i++) {
-    for (j=0; j<i+1;j++) {
-      document.write(a);
-    }
-    document.write('<br>');
+    var line = a.repeat(i+1);
+    document.write(line+'<br>');
   }
 };
+
+writeLetterManyTimes2();
